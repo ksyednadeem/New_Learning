a0906bf257bcbec4f01f2b571fed5e52a6996517
ksyednadeem <syednadeem165@gmail.com>
2024-07-12 23:41:29 +0530

    commit changes

--- If_Statements.py
+++ If_Statements.py
@@ -42,3 +42,32 @@ this is for if statements learning
 # for i in dict1.items():
 #     print(i)
 
+# ls=[1,2,3] # get out put as [1,3,9,16]
+# mul=1
+# for i in ls :
+#     mul=mul*i
+#     print(mul)
+
+# ls=[1,2,3] #[1,4,9]
+# sq=[]
+# for i in ls:
+#     sq.append(i*i)
+#     print (sq)
+
+# ls=[1,2,3,4,5,6,7,8,9,0]
+#
+# even_num=0
+# odd_num=0
+# for i in ls:
+#     if i%2==0:
+#         even_num = even_num + i
+#     else:
+#         odd_num=odd_num+i
+#
+# print(even_num)
+# print(odd_num)
+
+str='ETL Automation'
+
+for i in str:
+    if i.lower()==i
